ffc4c27e44e0c1ff7cb0e1e4ef9ac506ce323664
Erik Hansen <e.hansen31@live.com>
2018-06-23 13:38:00 -0400

    install absinthe for graphql

--- config/dev.exs
+++ config/dev.exs
@@ -49,6 +49,7 @@ config :logger, :console, format: "[$level] $message\n"
 config :phoenix, :stacktrace_depth, 20
 
 # Configure your database
+config :elixir_backend_sample, :db, name: "my_mongo_dev"
 # config :elixir_backend_sample, ElixirBackendSample.Repo,
 #   adapter: Ecto.Adapters.Postgres,
 #   username: "postgres",
--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule ElixirBackendSample.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:mongodb, ">= 0.0.0"},
+      {:absinthe, "~> 1.4.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
