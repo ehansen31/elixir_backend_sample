e9235844527569dce69e1a1407d8816995f2ddb3
Erik Hansen <e.hansen31@live.com>
2018-06-24 23:45:21 -0400

    begin changesets

--- lib/elixir_backend_sample_web/models/user.ex
+++ lib/elixir_backend_sample_web/models/user.ex
@@ -1,5 +1,8 @@
 defmodule ElixirBackendSampleWeb.Models.User do
   use Ecto.Schema
+  # This is the one that includes cast
+  import Ecto.Changeset
+  import Ecto.Query
 
   # # weather is the DB table
   # schema "weather" do
@@ -16,10 +19,28 @@ defmodule ElixirBackendSampleWeb.Models.User do
     field(:age, :integer)
   end
 
+  def changeset(user, params \\ %{}) do
+    user
+    |> cast(params, [:name, :email, :age])
+    |> validate_required([:email, :password])
+    |> validate_format(:email, ~r/@/)
+    |> validate_inclusion(:age, 18..100)
+    |> unique_constraint(:email)
+  end
+
   def create_user(userObj) do
     # check if the email exists in the database?
 
-    person = %ElixirBackendSampleWeb.Models.User{}
-    ElixirBackendSample.Repo.insert(person)
+    person = %ElixirBackendSampleWeb.Models.User{
+      email: userObj.email,
+      password: userObj.password,
+      first_name: userObj.first_name,
+      last_name: userObj.last_name,
+      age: userObj.age
+    }
+
+    {:ok, newUserObj} = ElixirBackendSample.Repo.insert(person)
+
+    # create a user token here?
   end
 end
